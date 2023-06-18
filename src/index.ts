01f60891121c20467cb0768e746a1e2b9910862e
Blaine <blaine-t@bathost.net>
2023-06-18 02:02:25 -0500

    Remove yarn to add fetch because typescript node

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+
 const app = express()
 const port = 42042
 
-import { router as api } from './routes/api'
+import { router as api } from './routes/api.js'
 
 app.use('/api', api)
+
+app.locals.HOST = 'https://unl.collegescheduler.com'
+app.locals.cookie = 'myCookie'
 
 app.listen(port, () => {
   console.log(`proxy server listening on http://127.0.0.1:${port}`)
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,34 @@
 import express from 'express'
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-    res.sendStatus(200)
+import fetch from 'node-fetch'
+
+router.get('/*', async (req, res) => {
+  // Check if cookie has not been initialized and tell user auth required
+  if (!req.app.locals.cookie) {
+    res.sendStatus(511)
+    return
+  }
+  const fetchArgs = {
+    method: req.method,
+    body: req.body,
+    headers: {
+      cookie: req.app.locals.cookie,
+    },
+  }
+  try {
+    const response = await fetch(
+      `${req.app.locals.HOST}/${req.originalUrl}`,
+      fetchArgs
+    )
+    if (!response.ok) {
+      throw new Error('Bad response from server')
+    }
+    res.send(await response)
+  } catch (err) {
+    console.error(err)
+    res.sendStatus(502)
+  }
 })
 
 export { router }
