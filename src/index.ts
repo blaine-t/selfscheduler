dafd49a1336e96b4a3250a12bb1f1c6ef67b01ad
Blaine <blaine-t@bathost.net>
2023-06-18 04:07:50 -0500

    Process.env complete overhaul

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express from 'express'
 import 'dotenv/config.js'
 
+// Initialize express
 const app = express()
-const port = 42042
+const port = process.env.PORT || 42042
 
 import { router as api } from './routes/api.js'
 
 app.use('/api', api)
 
-app.locals.HOST = 'https://unl.collegescheduler.com'
+if (!process.env.cookie) throw Error('No cookie provided')
+
 app.locals.cookie = process.env.cookie
+app.locals.HOST = process.env.HOST || 'https://unl.collegescheduler.com'
 app.locals.USERAGENT =
+  process.env.USERAGENT ||
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
 
 app.listen(port, () => {
