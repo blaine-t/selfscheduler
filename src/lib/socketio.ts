869b1575da9c711c5e002202819e0b7052d607b8
Blaine <blaine-t@bathost.net>
2023-06-19 04:05:45 -0500

    Socketio complete rehaul. Swap left. Prettier fix.

--- src/lib/socketio.ts
+++ src/lib/socketio.ts
@@ -4,50 +4,98 @@ import { app } from '../index'
 import query from './query'
 
 function connect() {
-  // Websocket only because *.collegescheduler.com doesn't support long polling
-  return io(app.locals.WS_HOST, {
+  // Websocket only because api.collegescheduler.com doesn't support long polling
+  const socket = io(app.locals.WS_HOST, {
     transports: ['websocket'],
   })
+  return socket
 }
 
-async function authorize(socket: SocketIOClient.Socket) {
-  // TODO: Add error handling
-  socket.emit(
-    'authorize',
-    { token: await query.accessToken() },
-    (authResponse: object) => {
+async function authorize(socket: SocketIOClient.Socket): Promise<void> {
+  const token = await query.accessToken()
+  return new Promise((resolve, reject) => {
+    socket.emit('authorize', { token }, (authResponse: object) => {
       // Check if callback says success true or false
       if (JSON.stringify(authResponse).includes('true')) {
-        return socket
+        console.log('Authorize')
+        resolve()
+      } else {
+        reject(new Error('Authorization failed'))
       }
-      return false
-    }
-  )
-}
-
-async function registerCart(termString: string) {
-  const socket = connect()
-  await authorize(socket)
-  const userInfo = await query.userInfo(termString)
-  const request = {
-    subdomain: 'unl',
-    type: 'ENROLL_CART',
-    userId: userInfo[0],
-    termCode: userInfo[1],
-    additionalData: { altPin: '' },
-  }
-  socket.emit('registration-request', request, (registerResponse: object) => {
-    if (JSON.stringify(registerResponse).includes('true')) {
-      socket.on('registration-response', (data: object) => {
-        socket.close()
-        console.log(JSON.stringify(data))
-        // TODO: call notification function
-      })
-    } else {
-      socket.close()
-    }
-    return false
+    })
   })
 }
 
-export default { registerCart }
+async function emit(requestType: string, request: object) {
+  const socket = connect()
+  await authorize(socket)
+  console.log('emit')
+  socket.emit(requestType, request, (callbackResponse: object) => {
+    listen(socket, requestType.replace('request', 'response'), callbackResponse)
+  })
+}
+
+function listen(
+  socket: SocketIOClient.Socket,
+  responseType: string,
+  callbackResponse: object
+) {
+  if (JSON.stringify(callbackResponse).includes('true')) {
+    socket.on(responseType, (response: object) => {
+      socket.close()
+      console.log(JSON.stringify(response))
+      // TODO: Add notifications
+    })
+  } else {
+    // TODO: Add error notification
+  }
+}
+
+function enroll(termCode: string) {
+  const request = {
+    subdomain: app.locals.SUBDOMAIN,
+    type: 'ENROLL_CART',
+    termCode,
+  }
+  emit('registration-request', request)
+}
+
+function drop(
+  regNumberList: string[],
+  termCode: string,
+  academicCareerCode: string
+) {
+  const request = {
+    regNumberRequests: [] as {
+      action: string
+      regNumber: string
+      academicCareerCode: string
+    }[],
+    subdomain: app.locals.SUBDOMAIN,
+    termCode,
+    type: 'EDIT',
+  }
+  regNumberList.forEach((regNumber) => {
+    request.regNumberRequests.push({
+      action: 'DROP',
+      regNumber,
+      academicCareerCode,
+    })
+  })
+  emit('registration-request', request)
+}
+
+function cart(classAction: Map<string, string>, termCode: string) {
+  const request = {
+    sections: [] as { regNumber: string; action: string }[],
+    termCode,
+    environment: app.locals.SUBDOMAIN,
+  }
+  // For each combination in classAction add that to be a new section in request
+  classAction.forEach((value, key) => {
+    request.sections.push({ regNumber: key, action: value })
+  })
+  emit('send-to-cart-request', request)
+}
+
+export default { enroll, drop, cart }
